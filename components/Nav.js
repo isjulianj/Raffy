f18a3b8c092ae4e529ec3e8f83f6726d846915c2
isjulianJaywing <julian.jackson@jaywing.com>
2019-07-20 12:52:05 +0100

    updated nav style

--- components/Nav.js
+++ components/Nav.js
@@ -3,12 +3,14 @@ import Link from "next/link";
 const linkStyle = {
   marginRight: 15
 };
-const linkStyle = {
-  marginRight: 15
+const navStyle = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center"
 };
 
 const Nav = () => (
-  <nav>
+  <nav style={navStyle}>
     <Link href="/">
       <a style={linkStyle}>Home</a>
     </Link>
